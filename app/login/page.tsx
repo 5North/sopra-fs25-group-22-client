954f3edeec52f8b57138d76a621eb102cdb60de6
shellmy.chakkaith <shellmy.chakkaith@uzh.ch>
2025-03-30 01:10:14 +0100

    Fix: removed unused variables

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
 import { useRouter } from "next/navigation"; // use NextJS router for navigation
-import { useApi } from "@/hooks/useApi";
+// import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 // import { User } from "@/types/user";
 import { Button, Form, Input } from "antd";
