9740ef8eac8d7a6e4e77f8ccbb7ca8be1b18582d
shellmy.chakkaith <shellmy.chakkaith@uzh.ch>
2025-05-05 22:58:48 +0200

    remove unused variable

--- app/scoreboard/page.tsx
+++ app/scoreboard/page.tsx
@@ -4,7 +4,6 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import {Button } from "antd";
 import { getUsers } from "@/api/registerService";
-import useLocalStorage from "@/hooks/useLocalStorage";
 
 interface UserGetDTO {
   id:         number;
